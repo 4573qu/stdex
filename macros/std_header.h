30c7a7a9dfb0e57b778805f44cb0423e304a9e9a
4573qu <4573quzhi@sina.com>
2025-10-10 23:51:11 +0800

    Add general std header

---
+++ macros/std_header.h
@@ -0,0 +1,43 @@
+//Last Modified At 2025/10/10
+//@Version 1.0.0.0
+#ifndef _STDEX_MACROS_STD_HEADER_H_
+#define _STDEX_UTILITY_MATCH_H_ 1
+
+namespace stdex {
+	namespace algorithm { }
+	namespace bitwise {	}
+	namespace machine {
+		namespace assembler { }
+	}
+	namespace math { }
+	namespace memory { }
+	namespace meta {
+		namespace core { }
+	}
+	namespace perf { }
+	namespace structure { }
+	namespace syntax { }
+	namespace type { }
+	namespace utility {	}
+	namespace vision { }
+}
+
+namespace std {
+
+	using namespace stdex::algorithm;
+	using namespace stdex::bitwise;
+	using namespace stdex::machine;
+	using namespace stdex::machine::assembler;
+	using namespace stdex::math;
+	using namespace stdex::memory;
+	using namespace stdex::meta::core;
+	using namespace stdex::perf;
+	using namespace stdex::structure;
+	using namespace stdex::syntax;
+	using namespace stdex::type;
+	using namespace stdex::utility;
+	using namespace stdex::vision;
+
+}
+
+#endif
